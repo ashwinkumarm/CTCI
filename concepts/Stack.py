5897dfde93c6b82485052f5129170f749d8b4be0
ashwinkumarm <ashbushster@gmail.com>
2018-06-10 02:57:37 -0500

    stack

---
+++ concepts/Stack.py
@@ -0,0 +1,27 @@
+
+'''
+Created on 10-Jun-2018
+
+@author: Ashwin
+'''
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+            
+    def isEmpty(self):
+        return len(self.items) == 0
+    
+    def push(self, x):
+        self.items.append(x)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items[len(self.items) - 1]
+    
+    def size(self):
+        return len(self.items)
+    
